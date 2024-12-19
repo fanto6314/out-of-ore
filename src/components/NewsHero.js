aac00a0e7b1b378caa9f56cd45bccb27405c63a9
fanto6314 <antonio.foresta04@gmail.com>
2024-12-19 14:22:25 +0100

    [~] Animation Fix

--- src/components/NewsHero.js
+++ src/components/NewsHero.js
@@ -31,69 +31,85 @@ export default function NewsHero() {
   useEffect(() => {
     const handleScroll = () => {
       const frontBlack = document.getElementById('front-black');
-      const scrollPosition = window.scrollY;
-      const opacity = Math.min(1, scrollPosition / 400);
-
-      frontBlack.style.backgroundColor = `rgba(21, 21, 21, ${0.6 + opacity * 0.4})`;
+      if (frontBlack) { // Check if the element exists
+        const scrollPosition = window.scrollY;
+        const opacity = Math.min(1, scrollPosition / 400);
+  
+        frontBlack.style.backgroundColor = `rgba(21, 21, 21, ${0.6 + opacity * 0.4})`;
+      }
     };
-
+  
+    // Call handleScroll immediately to set the initial opacity
+    handleScroll();
+  
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-
-  if (loading) {
-    return <div>Loading news...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  
+  
 
   const latestNews = newsItems[0];
 
   return (
     <section className="relative w-full h-[85vh]">
-      <Image className="absolute top-0 left-0 w-full h-full -mt-9 object-cover" src={latestNews.thumbnail} alt={latestNews.title} fill sizes="100vw" style={{objectFit: 'cover'}} />
+      {loading || error ? (
+        {/* h-full div with if loading return tailwind spinner loader & if error display in red text the error */},
+        <div className="h-full flex items-center justify-center">
+          {loading ? (
+            <div className="flex items-center justify-center">
+              <div className="w-16 h-16 border-4 border-yellow-400 border-t-transparent border-solid rounded-full animate-spin"></div>
+            </div>
+          ) : (
+            <div className="text-red-500 text-lg">
+              {error ? `Error: ${error.message}` : "An unexpected error occurred."}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className='relative w-full h-full'>
+          <Image className="absolute top-0 left-0 w-full h-full -mt-9 object-cover" src={latestNews.thumbnail} alt={latestNews.title} fill sizes="100vw" style={{objectFit: 'cover'}} />
 
-      <div className="relative w-full h-full">
-        <div className="absolute h-full flex flex-col items-start justify-center z-20 mx-4 2xl:mx-80 xl:mx-32">
-          <div className="text-start text-gray-100 text-base md:text-lg mb-3 bg-black/50 py-1 px-3 rounded-lg flex space-x-2 items-center">
-            <FaRegClock className='text-base' />
-            <span>
-              {new Date(latestNews.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
-            </span>
+          <div className="relative w-full h-full">
+            <div className="absolute h-full flex flex-col items-start justify-center z-20 mx-4 2xl:mx-80 xl:mx-32">
+              <div className="text-start text-gray-100 text-base md:text-lg mb-3 bg-black/50 py-1 px-3 rounded-lg flex space-x-2 items-center">
+                <FaRegClock className='text-base' />
+                <span>
+                  {new Date(latestNews.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+                </span>
+              </div>
+
+              <div className="text-start text-[#f8ce5e]">
+                <h1 className="bebas-neue text-[4rem] md:text-[6rem]">{latestNews.title}</h1>
+              </div>
+              <div className="text-start max-w-[650px]">
+                <p className="text-gray-300 text-base md:text-lg line-clamp-3">
+                  {latestNews.short}
+                </p>
+              </div>
+              <div className="text-start mt-6 flex row space-x-3">
+                <a href={latestNews.url} target="_blank" rel="noopener noreferrer" className="bg-[#f8ce5e] hover:bg-[#bd9e43] rounded-lg flex text-center items-center justify py-2 px-4">
+                  <span className="text-lg md:text-xl text-gray-900 bebas-neue-font">
+                    READ MORE
+                  </span>
+                </a>
+              </div>
+            </div>
           </div>
 
-          <div className="text-start text-[#f8ce5e]">
-            <h1 className="bebas-neue text-[4rem] md:text-[6rem]">{latestNews.title}</h1>
-          </div>
-          <div className="text-start max-w-[650px]">
-            <p className="text-gray-300 text-base md:text-lg line-clamp-3">
-              {latestNews.short}
-            </p>
-          </div>
-          <div className="text-start mt-6 flex row space-x-3">
-            <a href={latestNews.url} target="_blank" rel="noopener noreferrer" className="bg-[#f8ce5e] hover:bg-[#bd9e43] rounded-lg flex text-center items-center justify py-2 px-4">
-              <span className="text-lg md:text-xl text-gray-900 bebas-neue-font">
-                READ MORE
-              </span>
-            </a>
+          <div 
+            className="absolute top-0 left-0 w-full h-full z-10" 
+            id='front-black'
+            style={{ backgroundColor: 'rgba(21, 21, 21, 0.6)' }}
+          ></div>
+
+          <div className="absolute bottom-0 w-full">
+            <div className="relative text-center z-10 py-12 bg-[#0c0c0c]" id="shape">
+              <div className="dark-bar-pattern-4"></div>
+              <div className="dark-bar-pattern-4 bottom flip"></div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div 
-        className="absolute top-0 left-0 w-full h-full z-10" 
-        id='front-black'
-        style={{ backgroundColor: 'rgba(12, 12, 12)' }}
-      ></div>
-
-      <div className="absolute bottom-0 w-full">
-        <div className="relative text-center z-10 py-12 bg-[#0c0c0c]" id="shape">
-          <div className="dark-bar-pattern-4"></div>
-          <div className="dark-bar-pattern-4 bottom flip"></div>
-        </div>
-      </div>
+      )}
     </section>
   );
 }
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -8,7 +8,7 @@ export default function NewsList() {
   const [newsItems, setNewsItems] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const itemsPerPage = 9; // 3 rows x 3 columns
+  const itemsPerPage = 9;
   const router = useRouter();
   const searchParams = useSearchParams();
   const currentPage = parseInt(searchParams.get("newsPage") || "1", 10);
@@ -32,13 +32,16 @@ export default function NewsList() {
     fetchNews();
   }, []);
 
-  if (loading) {
-    return <div>Loading news...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  useEffect(() => {
+    if (window.location.hash === "#newsList") {
+      const target = document.querySelector("#newsList");
+      if (target) {
+        const offset = 80; // Adjust this value as needed
+        const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top: targetPosition, behavior: "smooth" });
+      }
+    }
+  }, [currentPage]);
 
   const totalPages = Math.ceil(newsItems.length / itemsPerPage);
   const paginatedNews = newsItems.slice(
@@ -48,96 +51,109 @@ export default function NewsList() {
 
   const handlePageChange = (page) => {
     if (page >= 1 && page <= totalPages) {
-      router.push(`?newsPage=${page}`);
+      router.push(`?newsPage=${page}#newsList`);
     }
   };
 
-  // {
-  //   "gid": "6350729003495608131",
-  //   "title": "Hotfix for Conversion message",
-  //   "url": "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/6350729003495608131",
-  //   "is_external_url": true,
-  //   "author": "northmoddingcompany",
-  //   "contents": "Hotfix to fix so the conversion message appears when loading all savegames made before 0.25 Best regards Christian",
-  //   "feedlabel": "Community Announcements",
-  //   "date": 1727994488,
-  //   "feedname": "steam_community_announcements",
-  //   "feed_type": 1,
-  //   "appid": 2009350,
-  //   "tags": [
-  //      "patchnotes"
-  //   ]
-  // },
-
   return (
     <section className="flex flex-col text-white z-50 h-full py-12 lg:mx-80 -mt-6">
-      {/* News list */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {paginatedNews.map((item) => (
-          <div className="flex flex-col">
-            <Link href={`/news/${item.id}`} key={item.id} rel="noopener noreferrer">
-              <img
-                src={item.thumbnail}
-                alt={item.title}
-                className="h-[250px] w-full object-cover rounded-lg"
-              />
-            </Link>
-            <div className="flex flex-row items-center mt-2 space-x-2 bebas-neue-font">
-              <div className="text-start text-gray-300 text-base md:text-xl bg-gray-800/50 py-1 px-3 rounded-lg">
-                <span>
-                  {new Date(item.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
-                </span>
+      {loading || error ? (
+        <div className="h-full flex items-center justify-center w-full">
+          {loading ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+              {Array.from({ length: itemsPerPage }).map((_, index) => (
+                <div key={index} className="flex flex-col animate-pulse">
+                  <div className="h-[250px] w-full bg-[#212121] rounded-lg"></div>
+                  <div className="flex flex-row items-center mt-2 space-x-2">
+                    <div className="w-40 h-6 bg-[#212121] rounded-lg"></div>
+                    <div className="w-20 h-6 bg-[#212121] rounded-lg"></div>
+                  </div>
+                  <div className="mt-3 h-8 bg-[#212121] rounded-lg"></div>
+                  <div className="mt-4 space-y-2">
+                    <div className="h-4 bg-[#212121] rounded-lg w-3/4"></div>
+                    <div className="h-4 bg-[#212121] rounded-lg w-full"></div>
+                    <div className="h-4 bg-[#212121] rounded-lg w-1/2"></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="text-red-500 text-lg">
+              {error ? `Error: ${error.message}` : "An unexpected error occurred."}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          {/* News list */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" id="newsList">
+            {paginatedNews.map((item) => (
+              <div className="flex flex-col">
+                <Link href={`/news/${item.id}`} key={item.id} rel="noopener noreferrer">
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    className="h-[250px] w-full object-cover rounded-lg"
+                  />
+                </Link>
+                <div className="flex flex-row items-center mt-2 space-x-2 bebas-neue-font">
+                  <div className="text-start text-gray-300 text-base md:text-xl bg-[#212121]/50 py-1 px-3 rounded-lg">
+                    <span>
+                      {new Date(item.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+                    </span>
+                  </div>
+                  <div className={`text-start text-base md:text-xl py-1 px-3 rounded-lg text-[#121212]/80 ${item.tags && item.tags.includes("patchnotes") ? 'bg-[#e55034] hover:bg-[#b83b22]' : 'bg-[#f8ce5e] hover:bg-[#bd9e43]'}`}> {/* Adjusted padding */}
+                    {item.tags && item.tags.includes("patchnotes") ? "PATCH" : "NEWS"}
+                  </div>
+                </div>
+                <div className="mt-3 text-start text-white uppercase bebas-neue-font tracking-wide text-base md:text-3xl">
+                  <Link href={`/news/${item.id}`} key={item.id} rel="noopener noreferrer">
+                    {item.title}
+                  </Link>
+                </div>
+                <div className="mt-4">
+                  {item.short}
+                </div>
               </div>
-              <div className={`text-start text-base md:text-xl py-1 px-3 rounded-lg text-[#121212]/80 ${item.tags && item.tags.includes("patchnotes") ? 'bg-[#e55034] hover:bg-[#b83b22]' : 'bg-[#f8ce5e] hover:bg-[#bd9e43]'}`}> {/* Adjusted padding */}
-                {item.tags && item.tags.includes("patchnotes") ? "PATCH" : "NEWS"}
-              </div>
-            </div>
-            <div className="mt-3 text-start text-white uppercase bebas-neue-font tracking-wide text-base md:text-3xl">
-              <Link href={`/news/${item.id}`} key={item.id} rel="noopener noreferrer">
-                {item.title}
-              </Link>
-            </div>
-            <div className="mt-4">
-              {item.short}
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      {/* Pagination */}
-      <div className="flex justify-center items-center mt-8 space-x-4">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-          className={`px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition ${
-            currentPage === 1 && "opacity-50 cursor-not-allowed"
-          }`}
-        >
-          Previous
-        </button>
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => handlePageChange(index + 1)}
-            className={`px-4 py-2 rounded-lg ${
-              currentPage === index + 1
-                ? "bg-[#f8ce5e] text-[#121212]"
-                : "bg-gray-700 hover:bg-gray-600"
-            } transition`}
-          >
-            {index + 1}
-          </button>
-        ))}
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          className={`px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition ${
-            currentPage === totalPages && "opacity-50 cursor-not-allowed"
-          }`}
-        >
-          Next
-        </button>
-      </div>
+          {/* Pagination */}
+          <div className="flex justify-center items-center mt-8 space-x-4">
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+              className={`px-4 py-2 rounded-lg bg-[#212121] hover:bg-gray-600 transition ${
+                currentPage === 1 && "opacity-50 cursor-not-allowed"
+              }`}
+            >
+              Previous
+            </button>
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => handlePageChange(index + 1)}
+                className={`px-4 py-2 rounded-lg ${
+                  currentPage === index + 1
+                    ? "bg-[#f8ce5e] text-[#121212]"
+                    : "bg-[#212121] hover:bg-gray-600"
+                } transition`}
+              >
+                {index + 1}
+              </button>
+            ))}
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className={`px-4 py-2 rounded-lg bg-[#212121] hover:bg-gray-600 transition ${
+                currentPage === totalPages && "opacity-50 cursor-not-allowed"
+              }`}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
